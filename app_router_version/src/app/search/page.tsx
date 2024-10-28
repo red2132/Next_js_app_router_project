16b8a2fb2fe3c79ff336974d2d99a2bdcdf9e34d
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-28 20:49:23 +0900

    ui 제작

--- app_router_version/src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-// url 파라미터 값이 Page의 props를 통해 전달됨
-export default async function Page({ searchParams }: { searchParams: Promise<{q: string}>}  ) {
-  const { q } = await searchParams;
-  return (
-    <div>search 페이지: { q }</div>
-  )
-}
